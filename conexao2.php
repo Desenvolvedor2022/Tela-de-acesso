a04c42dd4b1ddba5bb0c1419595c5cdaf1e89c2c
Jefter Almeida <103685683+Desenvolvedor2022@users.noreply.github.com>
2022-12-29 11:44:19 -0300

    Add files via upload

---
+++ conexao2.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+$localhost = "localhost";
+$user = "root";
+$passw = "";
+$banco = "log";
+
+global $pdo;
+
+try{
+// orientada objeto a pdo
+
+$pdo  = new PDO("mysql:dbname=".$banco."; host=".$localhost, $user, $passw);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch(PDOException $e){
+    echo "ERRO: ".$e->getMessage();
+    exit;
+}
+?>
